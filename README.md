a723d071230e8e153e2f089c581b3dafc2596194
Ninanwt <ninanwt19@gmail.com>
2023-12-05 00:05:52 +0700

    Add initial project files and configurations

---
+++ README.md
@@ -0,0 +1,13 @@
+# simple app for my capstone project using Express.JS and prisma 
+
+# Step by step 
+1. Inisiasi project npm `npm init -y`
+2. Update `package.json` dengan menambahkan scripts
+3. Install package yang diperlukan `npm install express mysql2 dotenv cors`
+4. Install decDependency `npm install -D nodemon`
+5. `node_modules` dan `package-lock.json` akan terinstall oleh `npm`
+6. Buat satu file `.gitignore` agar `node_modules` dan `.env` tidak ke upload ke git
+7. Buat satu file entrypoint untuk inisiasi project `index.js` setelah itu  bisa update `package.json`
+8. Import express, dotenv, dan package lain yang sudah di install tadi
+9. Buatlah satu rute untuk mencoba apakah aplikasi bisa jalan atau tidak
+10. Gunakan prisma agar kita bisa terhubung dengan database dan melakukan get/post data ke database dengan [prisma] (https://prisma.io)
--- index.js
+++ index.js
@@ -1,45 +1,23 @@
-// Import package express
-const express = require('express');
-const server = express();
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-//masih ambil data dari folder data bukan database
-const fs = require('fs');
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//import cors 
-const cors  = require('cors')
-
-
-//define port
-const PORT = 3001;
-
-//pakai cors agar bisa share resource antar backend dan frontend
-server.use(cors());
-
-
-//handle request di main routes ("/")
-server.get("/", function (request, response) {
-    //langsung kirim response aja tanpa membaca detail requestnya
-    response.send("Akan membalikan sebuah response");
+app.get("/", async (req, res) => {
+	res.send("here is the response");
 });
 
-//dari rute ini harapannya bisa mengirimkan data menu ke yang meminta request data menu
-server.get("/menu", (req, res) => {
-    //nanti proses logicnya itu ambil data dulu dari database, lalu dikirim melalui response, saat ini kita masih pake data dari json dulu/fake data
-    
-    //ambil data json dari /data/menu.json
-    fs.readFile('./data/menu.json', (error, data) => {
-        if (error) res.send("Gagal dalam pembacaan data");
-        const menu = JSON.parse(data);
-        res.status(200).send(menu);
-    });
+app.all("*", async (req, res) => {
+	res.json({
+		message: "Routes you're looking is not found",
+	});
 });
 
-//menangkap semua request/permintaan ke rute yang tidak dikenal
-server.all("*", (req, res) => {
-    res.status(404).send("404 not found");
-});
-
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-
+app.listen(PORT, "0.0.0.0", () => {
+	console.log(`Server is already running at ${PORT}`);
 });
